20a6ba55a965011d354b5d54e6e119a19ad82970
SreekanthReddy18 <sreekanth3143@gmail.com>
2019-04-13 16:13:05 +0530

    pickup th values from properties files.

--- src/main/java/com/srikanth/microservices/limitsservices/LimitsConfigurationController.java
+++ src/main/java/com/srikanth/microservices/limitsservices/LimitsConfigurationController.java
@@ -1,16 +1,21 @@
 package com.srikanth.microservices.limitsservices;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.srikanth.microservices.limitsservices.bean.Configuration;
 import com.srikanth.microservices.limitsservices.bean.LimitConfiguration;
 
 @RestController
 public class LimitsConfigurationController {
 	
+	@Autowired
+	private Configuration configuration;
+	
 	@GetMapping("/limits")
 	public LimitConfiguration retriveLimitsFromconfiguration() {
-		return new LimitConfiguration(1000, 10);
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
 
 }
---
+++ src/main/java/com/srikanth/microservices/limitsservices/bean/Configuration.java
@@ -0,0 +1,19 @@
+package com.srikanth.microservices.limitsservices.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Component
+@ConfigurationProperties("limits-service")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Configuration {
+
+	private int maximum;
+	private int minimum;
+}
