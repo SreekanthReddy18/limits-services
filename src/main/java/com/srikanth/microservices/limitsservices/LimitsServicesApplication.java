69c6a2a497b6c265acbc3a8a537221d2c18ebafc
SreekanthReddy18 <sreekanth3143@gmail.com>
2019-04-13 13:18:23 +0530

    test commit

--- src/main/java/com/srikanth/microservices/limitsservices/LimitsServicesApplication.java
+++ src/main/java/com/srikanth/microservices/limitsservices/LimitsServicesApplication.java
@@ -8,6 +8,7 @@ public class LimitsServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LimitsServicesApplication.class, args);
+		
 		System.out.println("Dummy");
 	}
 
