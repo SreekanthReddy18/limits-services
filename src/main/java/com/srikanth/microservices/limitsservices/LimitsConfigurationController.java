d4d7336069b0091aceaf6196f7ff0f6431f2743f
SreekanthReddy18 <sreekanth3143@gmail.com>
2019-04-18 12:25:06 +0530

    added Hystrix.

--- src/main/java/com/srikanth/microservices/limitsservices/LimitsConfigurationController.java
+++ src/main/java/com/srikanth/microservices/limitsservices/LimitsConfigurationController.java
@@ -1,9 +1,11 @@
 package com.srikanth.microservices.limitsservices;
 
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.srikanth.microservices.limitsservices.bean.Configuration;
 import com.srikanth.microservices.limitsservices.bean.LimitConfiguration;
 
@@ -17,5 +19,14 @@ public class LimitsConfigurationController {
 	public LimitConfiguration retriveLimitsFromconfiguration() {
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
-
+	
+	@GetMapping("/fault-tolerence-example")
+	//@HystrixCommand(fallbackMethod = "retriveLimits")
+	public LimitConfiguration retriveLimitsHystrix() {
+		throw new RuntimeException("Service not available");
+	}
+	
+	public LimitConfiguration retriveLimits() {
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
 }
--- src/main/java/com/srikanth/microservices/limitsservices/LimitsServicesApplication.java
+++ src/main/java/com/srikanth/microservices/limitsservices/LimitsServicesApplication.java
@@ -2,8 +2,11 @@ package com.srikanth.microservices.limitsservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+//import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+//@EnableHystrix
 public class LimitsServicesApplication {
 
 	public static void main(String[] args) {
